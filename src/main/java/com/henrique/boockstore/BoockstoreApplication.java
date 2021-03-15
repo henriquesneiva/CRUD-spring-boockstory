47017c0f44f58eb4e9e3b26c87bfefe2cd7d06f1
henriquesneiva <henriquesneiva@gmail.com>
2021-03-15 20:43:17 -0300

    criacao do projeto

---
+++ src/main/java/com/henrique/boockstore/BoockstoreApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.boockstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoockstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoockstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/boockstore/BoockstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.boockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
