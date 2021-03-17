0b5ea0d2575f9d1b7e4d21263e9eab6e5240dd53
henriquesneiva <henriquesneiva@gmail.com>
2021-03-17 13:09:55 -0300

    commit

--- src/main/java/com/henrique/boockstore/BoockstoreApplication.java
+++ src/main/java/com/henrique/boockstore/BoockstoreApplication.java
@@ -1,13 +1,41 @@
 package com.henrique.boockstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BoockstoreApplication {
+import com.henrique.boockstore.domain.Categoria;
+import com.henrique.boockstore.domain.Livro;
+import com.henrique.boockstore.repository.CategoriaRepository;
+import com.henrique.boockstore.repository.LivroRepository;
 
+@SpringBootApplication
+public class BoockstoreApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BoockstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica", "livros de ti");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robber M", "Loemdalskjdlkja", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
