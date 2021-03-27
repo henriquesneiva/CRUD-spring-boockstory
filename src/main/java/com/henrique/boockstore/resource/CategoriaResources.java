90271ced9dd6f27b396d1bbe098c9ef4984074d2
henriquesneiva <henriqueseniva@gmail.com>
2021-03-27 16:13:32 -0300

    commit

--- src/main/java/com/henrique/boockstore/resource/CategoriaResources.java
+++ src/main/java/com/henrique/boockstore/resource/CategoriaResources.java
@@ -1,13 +1,23 @@
 package com.henrique.boockstore.resource;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.henrique.boockstore.domain.Categoria;
+import com.henrique.boockstore.dto.CategoriaDTO;
 import com.henrique.boockstore.servise.CategoriaServise;
 
 @RestController
@@ -23,6 +33,33 @@ public class CategoriaResources {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDTO) {
+
+		Categoria newObj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delite(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/henrique/boockstore/servise/CategoriaServise.java
+++ src/main/java/com/henrique/boockstore/servise/CategoriaServise.java
@@ -1,11 +1,13 @@
 package com.henrique.boockstore.servise;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henrique.boockstore.domain.Categoria;
+import com.henrique.boockstore.dto.CategoriaDTO;
 import com.henrique.boockstore.repository.CategoriaRepository;
 import com.henrique.boockstore.servise.exceptions.ObjectNotFoundException;
 
@@ -23,4 +25,27 @@ public class CategoriaServise {
 
 	}
 
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
+		return repository.save(obj);
+		
+	}
+
+	public void delite(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 }
